72570ef9b999ca319e1861164ca95e361fdce98d
Sebastian Caceres <sebastiancaceres207@gmail.com>
2023-09-01 13:05:09 -0500

    navbar js funcionamiento del boton abrir - mostrar barra lateral  menu opciones

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const btnMenu = document.getElementById('btn-menu');
+const menu = document.getElementById('menu');
 
 btnMenu.addEventListener('click', () => {
-    console.log("Abrir")
+    menu.classList.toggle('show');
 });
