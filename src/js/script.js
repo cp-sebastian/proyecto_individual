753ac39fe435d029f29375c0da97c518809b1969
Sebastian Caceres <sebastiancaceres207@gmail.com>
2023-09-01 13:35:33 -0500

    navbar js desaparecer barra lateral y menu oscuro

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,14 @@ const btnMenu = document.getElementById('btn-menu');
 const menu = document.getElementById('menu');
 const darkBackground = document.getElementById('darkBackground')
 
+/*Al presionar el bóton menu, muestra barra lateral y oscurecer el fondo */
 btnMenu.addEventListener('click', () => {
-    menu.classList.toggle('show');
-    
+    menu.classList.add('show');
+    darkBackground.style.display = 'block';
+});
+
+/*Al hacer click en el fondo oscuro, ocultar barra lateral y fondo oscuro */
+darkBackground.addEventListener('click', () => {
+    menu.classList.remove('show');
+    darkBackground.style.display = 'none';
 });
