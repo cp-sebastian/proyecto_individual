18258c2e19d9ca8a70bc96055be81b268d1c864c
Sebastian Caceres <sebastiancaceres207@gmail.com>
2023-09-01 13:31:29 -0500

    navbar css mejorando el navbar y el fondo oscuro

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const btnMenu = document.getElementById('btn-menu');
 const menu = document.getElementById('menu');
+const darkBackground = document.getElementById('darkBackground')
 
 btnMenu.addEventListener('click', () => {
     menu.classList.toggle('show');
+    
 });
